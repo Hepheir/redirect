70141b7f42250241fe0762aad9c30dc7d3957507
Hepheir <hepheir@gmail.com>
2020-05-30 02:22:40 +0900

    test

--- dgu-eclass-vulnerable/build/src/linker.js
+++ dgu-eclass-vulnerable/build/src/linker.js
@@ -1,34 +1,34 @@
 try {
-    if (EclassUtils.isActive)
-        EclassUtils.unload();
+    if (document.EclassUtils.isActive)
+        document.EclassUtils.unload();
     else
-        EclassUtils.load();
+        document.EclassUtils.load();
 } catch {
-    var EclassUtils = class {
+    document.EclassUtils = class {
         static load() {
-            let base_url = EclassUtils.repository_url;
-            EclassUtils.downloadScript(base_url + 'app.js')
+            let base_url = document.EclassUtils.repository_url;
+            document.EclassUtils.downloadScript(base_url + 'app.js')
                 .then(eval)
                 .then(() => {
                     alert('이클래스 유틸리티 기능이 활성화 되었습니다.');
-                    EclassUtils.isActive = true;
+                    document.EclassUtils.isActive = true;
                 });
         }
 
         static load_vulnerable() {
             // 사용금지
-            let base_url = EclassUtils.repository_url;
-            EclassUtils.downloadScript(base_url + 'app-h.js')
+            let base_url = document.EclassUtils.repository_url;
+            document.EclassUtils.downloadScript(base_url + 'app-h.js')
                 .then(eval)
                 .then(() => {
                     alert('[주의] 이클래스 핵 기능이 활성화 되었습니다.');
-                    EclassUtils.isActive = true;
+                    document.EclassUtils.isActive = true;
                 });
         }
 
         static unload() {
             if (confirm('종료하시겠습니까?\n(확인을 누르시면 창이 새로고침 됩니다.)')) {
-                EclassUtils.isActive = false;
+                document.EclassUtils.isActive = false;
                 location.reload();
             }
         }
@@ -46,6 +46,6 @@ try {
             });
         }
     };
-    EclassUtils.repository_url = 'https://raw.githubusercontent.com/Hepheir/web_functions/master/dgu-eclass-vulnerable/';
-    EclassUtils.load();
+    document.EclassUtils.repository_url = 'https://raw.githubusercontent.com/Hepheir/web_functions/master/dgu-eclass-vulnerable/';
+    document.EclassUtils.load();
 }
