8729894bd71f9dc165ae389656f2319ce7999bf5
Hepheir <hepheir@gmail.com>
2020-06-01 08:52:12 +0900

    Create livestreaming for rasp

---
+++ livestream/index.py
@@ -0,0 +1,13 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('cam', frame)
+    key = cv2.waitKey(10)
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
